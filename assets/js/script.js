1599dba395cd6edc8a08d295e59924e9e93f3132
Lucas Fernandes <lucasfeitosa8@gmail.com>
2021-03-17 16:18:47 -0300

    Few changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Show and Hide Modal
+/* == Show and Hide Modal == */
 const modal = {
     open() {
         document.querySelector('.modal-overlay').classList.add('active')
@@ -6,5 +6,45 @@ const modal = {
 
     close() {
         document.querySelector('.modal-overlay').classList.remove('active')
-    }
+    },
 }
+
+// == Transações == 
+const transactions = [
+    {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/2021'
+    },
+
+    {
+    id: 2,
+    description: 'Website',
+    amount: 500000,
+    date: '23/01/2021'
+    },
+
+    {
+    id: 3,
+    description: 'Internet',
+    amount: -20000,
+    date: '23/01/2021'
+    },
+]
+
+
+/* == In and Out values == */
+const Transaction = {
+    incomes() {
+        // somar as entradas
+    },
+    expenses() {
+        // somar as saídas
+    },
+    total() {
+        // subtrair as saídas das entradas
+    },
+}
+
+// Captar itens do objeto Transactions e inserir no HTML
